13d02219c672dd337931c885d2d13d8a0637de72
Emre Elagoz <emreelagoz1994@gmail.com>
2020-10-09 09:57:16 +0200

    New Edit

---
+++ ReadKeyDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadKeyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key, please...");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine(key.Key.ToString());
+
+            if (key.Key.ToString() == "Enter")
+            {
+                Console.WriteLine("ENTER");
+            }
+            if (key.Key.ToString() == "LeftArrow")
+            {
+                Console.WriteLine("you pressed LeftArrow");
+            }
+        }
+    }
+}
